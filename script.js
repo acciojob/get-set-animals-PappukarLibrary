182be7f72ee226490748ed931f734fd1c9d46426
PappukarLibrary <158065740+PappukarLibrary@users.noreply.github.com>
2024-08-27 17:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Animal {}
+class Animal {
+	  constructor(private _species: string) {}
 
-class Dog extends Animal {}
+  // Getter for the species
+  get species(): string {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound the animal makes
+  makeSound(): void {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+	 constructor(species: string) {
+    super(species);
+  }
+
+  // Method to log "woof"
+  bark(): void {
+    console.log("woof");
+  }
+	
+}
+
+class Cat extends Animal {
+	 constructor(species: string) {
+    super(species);
+  }
+
+  // Method to log "purr"
+  purr(): void {
+    console.log("purr");
+  }
+}
+	
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
